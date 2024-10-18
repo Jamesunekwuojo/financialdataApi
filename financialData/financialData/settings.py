ee1310e790714d682c883fda70abd7f1ff665d12
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-10-18 09:49:41 +0100

    modify settings.py

--- financialData/financialData/settings.py
+++ financialData/financialData/settings.py
@@ -84,9 +84,20 @@ WSGI_APPLICATION = 'financialData.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-      
+       
+
+        'ENGINE': django.db.backends.postgresql
+
+        'NAME': '',
+
+        'USER': '',
+
+        'PASSWORD': '',
+
+        'HOST' : '',
+
+        'PORT' : '',
+
     }
 }
 
